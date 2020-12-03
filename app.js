2852944830f483ee1861d7b5b05e3d589647d735
sevde18 <sakarsevde@gmail.com>
2020-12-03 16:26:39 +0100

    03.12.2020 3th version

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-    
-      document.querySelector('.btn-new').addEventListener('click', function() { 
-        var weight = document.getElementById('weight').value;   
-        var height = document.getElementById('height').value; 
-        var bmi = weight / (height*height);
-       
-        bmiInterpratation(bmi);
+document.querySelector('.btn').addEventListener('click', function()
+ { 
+  var weight = document.getElementById('weight').value;   
+  var height = document.getElementById('height').value; 
+  var bmi = weight / (height*height);
+  if ( !isNaN(weight) && !isNaN(height) ) {
+    if(height > 2.50) { alert ("The hight is probably to high !");
+    } else {
+    bmiInterpratation(bmi);
+  }
+} else { alert ("weight and height must be numbers !!!"); }
+ 
+  bmiInterpratation(bmi);
 });
 
-function bmiInterpratation(bmi) {
-    var result;
-    if(bmi<18.5)
-    {result = " - underweight";}
-    else if (bmi>=18.5 && bmi<24.9) result = " - normal weight.";
-    else if (bmi>=25&& bmi< 29.0) result = " - overweight.";
-    else result = "- obesity";
-    document.getElementById('result').innerHTML = 
-    'Your BMI is ' + Math.round(bmi * 100) / 100 + ' ' + result;
+function bmiInterpratation(bmi) 
+{
+var result;
+if(bmi<18.5)
+{ 
+  result = " - underweight";
+  document.getElementById('result').classList.add("under-weight");
+}
+else if (bmi>=18.5 && bmi<24.9){result = " - normal weight.";
+document.getElementById('result').classList.add("normal-weight");
+} 
+else if (bmi>=25 && bmi< 29.0){ result = " - overweight.";
+document.getElementById('result').classList.add("over-weight");
+}
+else{
+  result = "- obesity";
+document.getElementById('result').classList.add("obesity");
+} 
+document.getElementById('result').innerHTML = 
+'Your BMI is ' + Math.round(bmi * 100) / 100 + ' ' + result;
 }
